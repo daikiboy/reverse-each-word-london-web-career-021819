d34faaf856e5c98f4defc2f93130ba382312525b
simone.rainieri <simone.rainieri@gmail.com>
2019-02-11 15:47:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,10 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
   new_array = []
-  array.each do |sentence|
+  array.collect do |sentence|
   new_array << sentence.reverse 
 end 
   return new_array.join(" ")
   end
   
 def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  new_array = []
-  array.collect {
